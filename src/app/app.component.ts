4275bb66ae1f52b50aee3c826a48e7c44aa95a3f
saulocapistrano <saulocapistrano@gmail.com>
2025-12-22 16:37:48 -0300

    Update app component: import additional Angular router modules and change title to 'Consulta de Créditos'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'search-credit-frontend';
+  title = 'Consulta de Créditos';
 }
